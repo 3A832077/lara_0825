eda4f837898f9e50407c6a115468e81a11ed6637
chen shu ting <a.0066715@gmail.com>
2021-11-13 22:19:52 +0800

    刪除多筆資料

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,9 @@ Route::get('/', function () {
     //$post=Post::find(1);
     //$post->delete();
 
-    Post::destroy(2);
+    //Post::destroy(2);
 
-    
+    Post::destroy(3,5,7);
 });
 
 Route::get('posts', [HomeController::class, 'index'])->name('posts.index');
