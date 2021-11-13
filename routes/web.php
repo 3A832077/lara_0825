8708ab467e9517463f6e3a4810d09af1d6a0d22d
chen shu ting <a.0066715@gmail.com>
2021-11-13 22:01:20 +0800

    改用 save的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    $post=new Post();
+    $post->title ='test title';
+    $post->content='test content';
+    $post->save();
+
 });
 
 Route::get('posts', [HomeController::class, 'index'])->name('posts.index');
