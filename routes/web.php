559b06745621b1b38e66c8d0e121f103ff81264b
chen shu ting <a.0066715@gmail.com>
2021-11-13 22:04:16 +0800

    使用 create方法新增資料

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    $post=new Post();
-    $post->title ='test title';
-    $post->content='test content';
-    $post->save();
+    //$post=new Post();
+    //$post->title ='test title';
+    //$post->content='test content';
+    //$post->save();
+
+    Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+    ]);
 
 });
 
