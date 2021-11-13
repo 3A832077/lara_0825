dab41e56db7f5fc90248c2c9d26e4687f97e8cc3
chen shu ting <a.0066715@gmail.com>
2021-11-13 22:13:06 +0800

    使用 find 方法查詢資料

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,12 @@ Route::get('/', function () {
         'title'=>'test title',
         'content'=>'test content',
     ]);
-    $posts=Post::all();
-    dd($posts);
+    //$posts=Post::all();
+    //dd($posts);
+
+    $post=Post::find(1);
+    dd($post);
+
 
 });
 
